29f3866690c72a0e2f0f426a9ab5ae501175eb27
aromatic32 <abdelrahman.wael1233@gmail.com>
2025-11-13 17:11:55 +0200

    updated routes with auth

--- routes/auth.js
+++ routes/auth.js
@@ -7,11 +7,13 @@ const {
   getOneUser,
   getAllUsers,
 } = require("../controllers/auth");
+const userRoleAuth = require("../middleware/userRoleAuth");
+const loginAuth = require("../middleware/loginAuth");
 
 router.post("/login", login);
 router.post("/register", register);
-router.delete("/delete/:id", deleteuser);
+router.delete("/delete/:id", loginAuth, deleteuser);
 router.get("/user/:id", getOneUser);
-router.get("/user", getAllUsers);
+router.get("/user", userRoleAuth, getAllUsers);
 
 module.exports = router;
