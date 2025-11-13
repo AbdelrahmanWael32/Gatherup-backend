89313dfcb961f5172092fdf6a88057cc2aad253f
basmala <basmalatarek21@gmail.com>
2025-11-13 16:59:24 +0200

    cors installtion

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
 const express = require("express");
+const cors = require("cors");
 const eventsRoute = require("../routes/events");
 const authRoute = require("../routes/auth");
 
@@ -8,6 +9,16 @@ const port = process.env.PORT || 7000;
 const LINK = process.env.DB_link;
 
 const app = express();
+app.use(express.json());
+
+app.use(
+  cors({
+    origin: "*",
+    methods: ["Get", "POST", "put", "Delete"],
+    CredentialS: true,
+  })
+);
+app.use(cors());
 app.use(express.json());
 
 mongoose
